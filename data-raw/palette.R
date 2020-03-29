0ee66eb3b11f0205695f40c785a4476a0d832263
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-03-29 11:20:52 +0200

    fix new mesh

--- data-raw/palette.R
+++ data-raw/palette.R
@@ -17,3 +17,14 @@ brain_pals = list(
   yeo17 = setNames(j$colour, j$region)
 )
 usethis::use_data(brain_pals, internal = TRUE, overwrite = TRUE)
+
+devtools::load_all("../../ggsegExtra/")
+yeo7_3d <- restruct_old_3datlas(yeo7_3d)
+yeo7_3d <- as_ggseg3d_atlas(yeo7_3d)
+usethis::use_data(yeo7_3d, internal = FALSE, overwrite = TRUE)
+
+
+yeo17_3d <- restruct_old_3datlas(yeo17_3d)
+yeo17_3d <- as_ggseg3d_atlas(yeo17_3d)
+usethis::use_data(yeo17_3d, internal = FALSE, overwrite = TRUE)
+
