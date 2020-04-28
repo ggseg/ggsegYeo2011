c4dc2a860ff34c3dfd93e60d8384c8ff2c19b28d
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-04-28 15:54:46 +0200

    fix bugged yeo17 atlases

--- data-raw/palette.R
+++ data-raw/palette.R
@@ -11,7 +11,7 @@ brain_pals <- c(yeo7_pal, yeo17_pal)
 usethis::use_data(brain_pals, internal = TRUE, overwrite = TRUE)
 devtools::load_all(".")
 
-# make atlas ----
+# yeo7 ----
 # Not working nicely. some strange polygon stuff hannening.
 yeo7 <- ggsegExtra::make_ggseg3d_2_ggseg(yeo7_3d,
                                          steps = 6:7,
@@ -30,10 +30,20 @@ usethis::use_data(yeo7,
                   overwrite = TRUE,
                   compress="xz")
 
+
+
+# yeo17 ----
+yeo17_3d <- ggsegExtra::make_aparc_2_3datlas(annot = "Yeo2011_17Networks_N1000")
+yeo17_3d <- as_ggseg3d_atlas(yeo17_3d)
+yeo17_3d <- mutate(yeo17_3d, atlas = "yeo17_3d")
+ggseg3d::ggseg3d(atlas = yeo17_3d, hemisphere = "right")
+ggseg3d::ggseg3d(atlas = yeo17_3d, hemisphere = "left")
+
 yeo17 <- ggsegExtra::make_ggseg3d_2_ggseg(yeo17_3d,
                                           steps = 6:7,
-                                         tolerance = .1,
-                                          output_dir = "~/Desktop/test/")
+                                          tolerance = .2,
+                                          output_dir = "data-raw") %>%
+  mutate(region = ifelse(grepl("Wall", region), NA, region))
 
 yeo17$geometry <- NULL
 yeo17 <- as_ggseg_atlas(yeo17)
@@ -45,7 +55,7 @@ ggseg(atlas=yeo17, #show.legend = FALSE,
   scale_fill_brain("yeo17", package = "ggsegYeo2011")
 
 
-usethis::use_data(yeo17,
+usethis::use_data(yeo17,yeo17_3d,
                   internal = FALSE,
                   overwrite = TRUE,
                   compress="xz")
